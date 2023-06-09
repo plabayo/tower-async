e3809de46295a1098ba8b92ef7cef9af12c267d8
glendc <glen.decauwsemaecker@otainsight.com>
2023-06-09 14:55:46 +0200

    improve docs and renames

--- tower-async/src/lib.rs
+++ tower-async/src/lib.rs
@@ -12,12 +12,22 @@
 
 //! `async fn(Request) -> Result<Response, Error>`
 //!
-//! # Overview
+//! ## Fork
 //!
-//! Tower is a library of modular and reusable components for building
-//! robust networking clients and servers.
+//! Tower Async is a fork of <https://github.com/tower-rs/tower>
+//! and makes use of `async traits` to simplify things and make it more easier
+//! to integrate async functions into middleware.
 //!
-//! Tower provides a simple core abstraction, the [`Service`] trait, which
+//! Where suitable we'll keep in sync (manually) with Tower and if the
+//! opportunity arises we'll contribute back "upstream" as well.
+//!
+//! ## Overview
+//!
+//! Tower Async aims to make it as easy as possible to build robust networking clients and
+//! servers. It is protocol agnostic, but is designed around a request / response
+//! pattern. If your protocol is entirely stream based, Tower may not be a good fit.
+//!
+//! Tower Async provides a simple core abstraction, the [`Service`] trait, which
 //! represents an asynchronous function taking a request and returning either a
 //! response or an error. This abstraction can be used to model both clients and
 //! servers.
@@ -35,28 +45,28 @@
 //! different type. The [`ServiceBuilder`] type is used to add middleware to a
 //! service by composing it with multiple [`Layer`]s.
 //!
-//! ## The Tower Ecosystem
+//! ### The Tower Ecosystem
 //!
 //! Tower is made up of the following crates:
 //!
-//! * [`tower`] (this crate)
-//! * [`tower-service`]
-//! * [`tower-layer`]
-//! * [`tower-test`]
+//! * [`tower-async`] (this crate)
+//! * [`tower-async-service`]
+//! * [`tower-async-layer`]
+//! * [`tower-async-test`]
 //!
 //! Since the [`Service`] and [`Layer`] traits are important integration points
 //! for all libraries using Tower, they are kept as stable as possible, and
 //! breaking changes are made rarely. Therefore, they are defined in separate
-//! crates, [`tower-service`] and [`tower-layer`]. This crate contains
+//! crates, [`tower-async-service`] and [`tower-async-layer`]. This crate contains
 //! re-exports of those core traits, implementations of commonly-used
 //! middleware, and [utilities] for working with [`Service`]s and [`Layer`]s.
-//! Finally, the [`tower-test`] crate provides tools for testing programs using
+//! Finally, the [`tower-async-test`] crate provides tools for testing programs using
 //! Tower.
 //!
-//! # Usage
+//! ## Usage
 //!
 //! Tower provides an abstraction layer, and generic implementations of various
-//! middleware. This means that the `tower` crate on its own does *not* provide
+//! middleware. This means that the `tower-async` crate on its own does *not* provide
 //! a working implementation of a network client or server. Instead, Tower's
 //! [`Service` trait][`Service`] provides an integration point between
 //! application code, libraries providing middleware implementations, and
@@ -67,7 +77,7 @@
 //!
 //! * **Implementing application logic** for a networked program. You might
 //!   use the [`Service`] trait to model your application's behavior, and use
-//!   the middleware [provided by this crate](#modules) and by other libraries
+//!   the middleware [provided by this crate][all_layers] and by other libraries
 //!   to add functionality to clients and servers provided by one or more
 //!   protocol implementations.
 //! * **Implementing middleware** to add custom behavior to network clients and
@@ -76,7 +86,7 @@
 //!   other Tower users!) or application-specific behavior that needs to be
 //!   shared between multiple clients or servers.
 //! * **Implementing a network protocol**. Libraries that implement network
-//!   protocols (such as HTTP) can depend on `tower-service` to use the
+//!   protocols (such as HTTP) can depend on `tower-async-service` to use the
 //!   [`Service`] trait as an integration point between the protocol and user
 //!   code. For example, a client for some protocol might implement [`Service`],
 //!   allowing users to add arbitrary Tower middleware to those clients.
@@ -84,39 +94,22 @@
 //!
 //!   Additionally, when a network protocol requires functionality already
 //!   provided by existing Tower middleware, a protocol implementation might use
-//!   Tower middleware internally, as well as as an integration point.
+//!   Tower middleware internally, as well as an integration point.
 //!
-//! ## Library Support
+//! ### Library Support
 //!
-//! A number of third-party libraries support Tower and the [`Service`] trait.
-//! The following is an incomplete list of such libraries:
+//! Following are some libraries that make use of Tower Async (instead of Tower)
+//! and the [`Service`] trait:
 //!
-//! * [`hyper`]: A fast and correct low-level HTTP implementation.
-//! * [`tonic`]: A [gRPC-over-HTTP/2][grpc] implementation built on top of
-//!   [`hyper`]. See [here][tonic-examples] for examples of using [`tonic`] with
-//!   Tower.
-//! * [`warp`]: A lightweight, composable web framework. See
-//!   [here][warp-service] for details on using [`warp`] with Tower.
-//! * [`tower-lsp`]: implementations of the [Language
-//!   Server Protocol][lsp] based on Tower.
+//! * [`rama`]: A proxy framework to anonymise your network traffic.
 //!
-//! [`hyper`]: https://crates.io/crates/hyper
-//! [`tonic`]: https://crates.io/crates/tonic
-//! [tonic-examples]: https://github.com/hyperium/tonic/tree/master/examples/src/tower
-//! [grpc]: https://grpc.io
-//! [`warp`]: https://crates.io/crates/warp
-//! [warp-service]: https://docs.rs/warp/0.2.5/warp/fn.service.html
-//! [`tower-lsp`]: https://crates.io/crates/tower-lsp
-//! [lsp]: https://microsoft.github.io/language-server-protocol/
+//! [`rama`]: https://crates.io/crates/rama
 //!
-//! If you're the maintainer of a crate that supports Tower, we'd love to add
+//! If you're the maintainer of a crate that supports Tower Async, we'd love to add
 //! your crate to this list! Please [open a PR] adding a brief description of
 //! your library!
 //!
-//! ## Getting Started
-//!
-//! If you're brand new to Tower and want to start with the basics, we recommend you
-//! check out some of our [guides].
+//! ### Getting Started
 //!
 //! The various middleware implementations provided by this crate are feature
 //! flagged, so that users can only compile the parts of Tower they need. By
@@ -136,30 +129,34 @@
 //! tower = { version = "0.4", features = ["retry", "timeout"] }
 //! ```
 //!
-//! See [here](#modules) for a complete list of all middleware provided by
+//! See [here][all_layers] for a complete list of all middleware provided by
 //! Tower.
 //!
+//! [`Service`]: https://docs.rs/tower-async/latest/tower-async/trait.Service.html
+//! [`Layer`]: https://docs.rs/tower-async/latest/tower-async/trait.Layer.html
+//! [all_layers]: https://docs.rs/tower-async/latest/tower-async/#modules
+//! [timeouts]: https://docs.rs/tower-async/latest/tower-async/timeout/
+//! [rate limiting]: https://docs.rs/tower-async/latest/tower-async/limit/rate
+//! [load balancing]: https://docs.rs/tower-async/latest/tower-async/balance/
+//! [`ServiceBuilder`]: https://docs.rs/tower-async/latest/tower-async/struct.ServiceBuilder.html
+//! [utilities]: https://docs.rs/tower-async/latest/tower-async/trait.ServiceExt.html
+//! [`tower-async`]: https://crates.io/crates/tower
+//! [`tower-async-service`]: https://crates.io/crates/tower-async-service
+//! [`tower-async-layer`]: https://crates.io/crates/tower-async-layer
+//! [`tower-async-test`]: https://crates.io/crates/tower-async-test
+//! [`retry`]: https://docs.rs/tower-async/latest/tower-async/retry
+//! [open a PR]: https://github.com/plabayo/tower-async/compare
 //!
 //! ## Supported Rust Versions
 //!
-//! Tower will keep a rolling MSRV (minimum supported Rust version) policy of **at
-//! least** 6 months. When increasing the MSRV, the new Rust version must have been
-//! released at least six months ago. The current MSRV is 1.49.0.
+//! Tower Async requires nightly Rust for the time being and has no backwards compatibility
+//! promises for the time being.
 //!
-//! [`Service`]: crate::Service
-//! [`Layer`]: crate::Layer
-//! [timeouts]: crate::timeout
-//! [rate limiting]: crate::limit::rate
-//! [load balancing]: crate::balance
-//! [`ServiceBuilder`]: crate::ServiceBuilder
-//! [utilities]: crate::ServiceExt
-//! [`tower`]: https://crates.io/crates/tower
-//! [`tower-service`]: https://crates.io/crates/tower-service
-//! [`tower-layer`]: https://crates.io/crates/tower-layer
-//! [`tower-test`]: https://crates.io/crates/tower-test
-//! [`retry`]: crate::retry
-//! [open a PR]: https://github.com/plabayo/async-tower/compare
-//! [guides]: https://github.com/plabayo/async-tower/tree/master/guides
+//! Once `async traits` are stabalized we'll start supporting stable rust once again,
+//! and we can start working towards backwards compatibility.
+//!
+//! Read <https://blog.rust-lang.org/inside-rust/2023/05/03/stabilizing-async-fn-in-trait.html> for more information
+//! on this roadmap by the Rust Language Core Team.
 
 #[macro_use]
 pub(crate) mod macros;
