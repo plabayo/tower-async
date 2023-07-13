831684de36b1ba6e27ca86779cca3d67aa0aa439
glendc <glen.decauwsemaecker@otainsight.com>
2023-07-13 15:14:20 +0200

    renable+port the remaining E2E retry (layer) tests

--- tower-async/tests/retry/main.rs
+++ tower-async/tests/retry/main.rs
@@ -21,100 +21,83 @@ async fn retry_errors() {
         .expect_response("world");
 }
 
-// #[tokio::test(flavor = "current_thread")]
-// async fn retry_limit() {
-//     let _t = support::trace_init();
+#[tokio::test(flavor = "current_thread")]
+async fn retry_limit() {
+    let _t = support::trace_init();
 
-//     let (mut service, mut handle) = new_service(Limit(2));
+    Builder::new("hello")
+        .send_error("retry 1")
+        .expect_request("hello")
+        .send_error("retry 2")
+        .expect_request("hello")
+        .send_error("retry 3")
+        .expect_request("hello")
+        .test(RetryLayer::new(Limit(2)))
+        .await
+        .expect_error("retry 3");
+}
 
-//     assert_ready_ok!(service.poll_ready());
+#[tokio::test(flavor = "current_thread")]
+async fn retry_error_inspection() {
+    let _t = support::trace_init();
 
-//     let mut fut = task::spawn(service.call("hello"));
+    Builder::new("hello")
+        .send_error("retry 1")
+        .expect_request("hello")
+        .send_error("reject")
+        .expect_request("hello")
+        .test(RetryLayer::new(UnlessErr("reject")))
+        .await
+        .expect_error("reject");
+}
 
-//     assert_request_eq!(handle, "hello").send_error("retry 1");
-//     assert_pending!(fut.poll());
+#[tokio::test(flavor = "current_thread")]
+async fn retry_cannot_clone_request() {
+    let _t = support::trace_init();
 
-//     assert_request_eq!(handle, "hello").send_error("retry 2");
-//     assert_pending!(fut.poll());
+    Builder::new("hello")
+        .send_error("retry 1")
+        .expect_request("hello")
+        .test(RetryLayer::new(CannotClone))
+        .await
+        .expect_error("retry 1");
+}
 
-//     assert_request_eq!(handle, "hello").send_error("retry 3");
-//     assert_eq!(assert_ready_err!(fut.poll()).to_string(), "retry 3");
-// }
+#[tokio::test(flavor = "current_thread")]
+async fn success_with_cannot_clone() {
+    let _t = support::trace_init();
 
-// #[tokio::test(flavor = "current_thread")]
-// async fn retry_error_inspection() {
-//     let _t = support::trace_init();
+    // Even though the request couldn't be cloned, if the first request succeeds,
+    // it should succeed overall.
+    Builder::new("hello")
+        .send_response("world")
+        .test(RetryLayer::new(CannotClone))
+        .await
+        .expect_response("world");
+}
 
-//     let (mut service, mut handle) = new_service(UnlessErr("reject"));
+#[tokio::test(flavor = "current_thread")]
+async fn retry_mutating_policy() {
+    let _t = support::trace_init();
 
-//     assert_ready_ok!(service.poll_ready());
-//     let mut fut = task::spawn(service.call("hello"));
-
-//     assert_request_eq!(handle, "hello").send_error("retry 1");
-//     assert_pending!(fut.poll());
-
-//     assert_request_eq!(handle, "hello").send_error("reject");
-//     assert_eq!(assert_ready_err!(fut.poll()).to_string(), "reject");
-// }
-
-// #[tokio::test(flavor = "current_thread")]
-// async fn retry_cannot_clone_request() {
-//     let _t = support::trace_init();
-
-//     let (mut service, mut handle) = new_service(CannotClone);
-
-//     assert_ready_ok!(service.poll_ready());
-//     let mut fut = task::spawn(service.call("hello"));
-
-//     assert_request_eq!(handle, "hello").send_error("retry 1");
-//     assert_eq!(assert_ready_err!(fut.poll()).to_string(), "retry 1");
-// }
-
-// #[tokio::test(flavor = "current_thread")]
-// async fn success_with_cannot_clone() {
-//     let _t = support::trace_init();
-
-//     // Even though the request couldn't be cloned, if the first request succeeds,
-//     // it should succeed overall.
-//     let (mut service, mut handle) = new_service(CannotClone);
-
-//     assert_ready_ok!(service.poll_ready());
-//     let mut fut = task::spawn(service.call("hello"));
-
-//     assert_request_eq!(handle, "hello").send_response("world");
-//     assert_ready_ok!(fut.poll(), "world");
-// }
-
-// #[tokio::test(flavor = "current_thread")]
-// async fn retry_mutating_policy() {
-//     let _t = support::trace_init();
-
-//     let (mut service, mut handle) = new_service(MutatingPolicy { remaining: 2 });
-
-//     assert_ready_ok!(service.poll_ready());
-//     let mut fut = task::spawn(service.call("hello"));
-
-//     assert_request_eq!(handle, "hello").send_response("world");
-//     assert_pending!(fut.poll());
-//     // the policy alters the request. in real life, this might be setting a header
-//     assert_request_eq!(handle, "retrying").send_response("world");
-
-//     assert_pending!(fut.poll());
-
-//     assert_request_eq!(handle, "retrying").send_response("world");
-
-//     assert_ready_err!(fut.poll(), "out of retries");
-// }
-
-type Req = &'static str;
-type Res = &'static str;
-type InnerError = &'static str;
-type Error = &'static str;
+    Builder::new("hello")
+        .send_error("retry 1")
+        .expect_request("hello")
+        .send_response("world")
+        .expect_request("retrying")
+        .send_response("world")
+        .test(RetryLayer::new(MutatingPolicy { remaining: 2 }))
+        .await
+        .expect_error("out of retries");
+}
 
 #[derive(Debug, Clone, PartialEq)]
 struct RetryErrors;
 
-impl Policy<Req, Res, Error> for RetryErrors {
+impl<Req, Res, Error> Policy<Req, Res, Error> for RetryErrors
+where
+    Req: Copy,
+{
     async fn retry(&mut self, _: &mut Req, result: &mut Result<Res, Error>) -> bool {
         result.is_err()
     }
@@ -127,7 +110,10 @@ impl Policy<Req, Res, Error> for RetryErrors {
 #[derive(Debug, Clone, PartialEq)]
 struct Limit(usize);
 
-impl Policy<Req, Res, Error> for Limit {
+impl<Req, Res, Error> Policy<Req, Res, Error> for Limit
+where
+    Req: Copy,
+{
     async fn retry(&mut self, _: &mut Req, result: &mut Result<Res, Error>) -> bool {
         if result.is_err() && self.0 > 0 {
             self.0 -= 1;
@@ -143,15 +129,19 @@ impl Policy<Req, Res, Error> for Limit {
 }
 
 #[derive(Debug, Clone, PartialEq)]
-struct UnlessErr(InnerError);
+struct UnlessErr<Error>(Error);
 
-impl Policy<Req, Res, Error> for UnlessErr {
+impl<Req, Res, Error> Policy<Req, Res, Error> for UnlessErr<Error>
+where
+    Error: ToString,
+    Req: Copy,
+{
     async fn retry(&mut self, _: &mut Req, result: &mut Result<Res, Error>) -> bool {
         result
             .as_ref()
             .err()
             .and_then(|err| {
-                if err.to_string() != self.0 {
+                if err.to_string() != self.0.to_string() {
                     Some(())
                 } else {
                     None
@@ -168,7 +158,7 @@ impl Policy<Req, Res, Error> for UnlessErr {
 #[derive(Debug, Clone, PartialEq)]
 struct CannotClone;
 
-impl Policy<Req, Res, Error> for CannotClone {
+impl<Req, Res, Error> Policy<Req, Res, Error> for CannotClone {
     async fn retry(&mut self, _: &mut Req, _: &mut Result<Res, Error>) -> bool {
         unreachable!("retry cannot be called since request isn't cloned");
     }
@@ -185,11 +175,11 @@ struct MutatingPolicy {
     remaining: usize,
 }
 
-impl Policy<Req, Res, Error> for MutatingPolicy
+impl<Res, Error> Policy<&'static str, Res, Error> for MutatingPolicy
 where
     Error: From<&'static str>,
 {
-    async fn retry(&mut self, req: &mut Req, result: &mut Result<Res, Error>) -> bool {
+    async fn retry(&mut self, req: &mut &'static str, result: &mut Result<Res, Error>) -> bool {
         if self.remaining == 0 {
             *result = Err("out of retries".into());
             false
@@ -200,7 +190,7 @@ where
         }
     }
 
-    fn clone_request(&mut self, req: &Req) -> Option<Req> {
+    fn clone_request(&mut self, req: &&'static str) -> Option<&'static str> {
         Some(*req)
     }
 }
